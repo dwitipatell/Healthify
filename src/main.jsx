6dff5563c9dce9273d3edd497b191a5643345898
Vishakhaa-22 <nebhananiv@gmail.com>
2026-04-03 22:19:20 +0530

    main.jsx no change

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-<<<<<<< HEAD
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
@@ -10,11 +9,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </BrowserRouter>
 )
-=======
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
->>>>>>> 40970e347bdc6c6841b6f49292637f701a4e4505
